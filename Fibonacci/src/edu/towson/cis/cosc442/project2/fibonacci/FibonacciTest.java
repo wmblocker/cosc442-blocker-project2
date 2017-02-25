14becd6ce90b93cfd1daf45c16dc9de0663ba730
wblock1 <wblock1@Williams-MacBook-Pro.local>
2017-02-25 17:25:20 -0500

    Updated FibonacciTest

--- Fibonacci/src/edu/towson/cis/cosc442/project2/fibonacci/FibonacciTest.java
+++ Fibonacci/src/edu/towson/cis/cosc442/project2/fibonacci/FibonacciTest.java
@@ -31,13 +31,13 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacci() {	
-		assertEquals("0", 0, fibonacci.fibonacci(0));
+		assertEquals("0", 1, fibonacci.fibonacci(0));
 		assertEquals("1", 1, fibonacci.fibonacci(1));
-		assertEquals("2", 1, fibonacci.fibonacci(2));
-		assertEquals("3", 2, fibonacci.fibonacci(3));
-		assertEquals("4", 3, fibonacci.fibonacci(4));
-		assertEquals("5", 5, fibonacci.fibonacci(5));
-		assertEquals("6", 8, fibonacci.fibonacci(6));
-		assertEquals("7", 13, fibonacci.fibonacci(7));
+		assertEquals("2", 2, fibonacci.fibonacci(2));
+		assertEquals("3", 3, fibonacci.fibonacci(3));
+		assertEquals("4", 5, fibonacci.fibonacci(4));
+		assertEquals("5", 8, fibonacci.fibonacci(5));
+		assertEquals("6", 13, fibonacci.fibonacci(6));
+		assertEquals("7", 21, fibonacci.fibonacci(7));
 	}
 }
